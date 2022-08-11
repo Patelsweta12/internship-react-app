f871f893b0b6869447dddfac606c034680d3a4de
Patelsweta12 <swetapatel11114@gmail.com>
2022-08-11 23:00:29 +0530

    Updates

---
+++ precache-manifest.33b97257f271d48b1561cde81a46ac82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "82398c46ef7f7dc083a7",
+    "url": "/static/css/main.0f4f0f71.chunk.css"
+  },
+  {
+    "revision": "82398c46ef7f7dc083a7",
+    "url": "/static/js/main.82398c46.chunk.js"
+  },
+  {
+    "revision": "5dba5810f30f5bf29e97",
+    "url": "/static/js/1.5dba5810.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "22e350b5423abb3783e46451096b0c9b",
+    "url": "/static/media/mode-icon.22e350b5.svg"
+  },
+  {
+    "revision": "289de1e14de4a84acc40bda2c87d528e",
+    "url": "/index.html"
+  }
+];
